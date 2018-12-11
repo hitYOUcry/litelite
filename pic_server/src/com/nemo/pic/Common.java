b3e05da26e2dcdc2155d86dc91879abe0639f962
285692817@qq.com <285692817@qq.com>
2018-12-11 21:41:02 +0800

    fix server url

--- pic_server/src/com/nemo/pic/Common.java
+++ pic_server/src/com/nemo/pic/Common.java
@@ -1,5 +1,6 @@
 package com.nemo.pic;
 
 public class Common {
-    public static final String SERVER_URL = "http://localhost:8080";
+//    public static final String SERVER_URL = "http://localhost:8080";
+    public static final String SERVER_URL = "https://nemozhang.xyz";
 }
